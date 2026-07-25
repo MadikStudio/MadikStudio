3a356f2fe82cf51031afa53e9d99a260069f9ec0
MadikStudio <muhamedkupov@gmail.com>
2026-07-25 16:38:53 +0300

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://madikstudio.github.io',
+  base: '/MadikStudio',
+  output: 'static',
+  trailingSlash: 'always',
+});
