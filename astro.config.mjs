6a54b04c6a4ea9d1145ff1a369bffec69fd60850
MadikStudio <muhamedkupov@gmail.com>
2026-07-25 22:25:31 +0300

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://madikstudio.github.io',
-  base: '/MadikStudio',
-  output: 'static',
-  trailingSlash: 'always',
-});
